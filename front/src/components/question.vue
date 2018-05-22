8c11a6157dbaa712160995407e0d357039bc89b9
nozomi0966 <nozomotoitech@gmail.com>
2018-05-22 13:43:44 +0900

    CSSを修正

--- front/src/components/question.vue
+++ front/src/components/question.vue
@@ -1,21 +1,28 @@
 <template>
     <div id='ques' style="width: 100%;">
-        <p>What did you use?</p>
-        <input type="checkbox" value="3DPrinter" v-model="checkedUse">
-        <label for="3DPrinter">3DPrinter</label>
-        <input type="checkbox" value="LaserCutter" v-model="checkedUse">
-        <label for="LaserCutter">LaserCutter</label>
-        <input type="checkbox" value="Other" v-model="checkedUse">
-        <label for="Other">Other</label>
-
-        <p>If you have any request please fill in</p>
+      <div class="question">
+        <h2>What did you use?</h2>
+        <div class="checkboxes alignment" data-toggle="buttons-radio">
+          <div class="checkbox">
+            <input class="btn btn-info" type="checkbox" value="3DPrinter" v-model="checkedUse">
+            <label for="3DPrinter" style="font-size: 20px">3DPrinter</label>
+          </div>
+          <div class="checkbox">
+            <input class="btn btn-info" type="checkbox" value="LaserCutter" v-model="checkedUse">
+            <label for="LaserCutter" style="font-size: 20px">LaserCutter</label>
+          </div>
+          <div class="checkbox">
+            <input class="btn btn-info" type="checkbox" value="Other" v-model="checkedUse">
+            <label for="Other" style="font-size: 20px">Other</label>
+          </div>
+        </div>
+        <h3>If you have any request please fill in.</h3>
         <p style="white-space: pre-line;">{{ message }}</p>
         <br>
         <textarea v-model="message" placeholder="add multiple lines"></textarea>
         <br>
-
-        <button v-on:click="send">send</button>
-        <span>Checked names: {{ checkedUse }}</span>
+        <button class="btn btn-info" v-on:click="send">send</button>
+      </div>
     </div>
 </template>
 
@@ -61,7 +68,7 @@ export default {
 
 <style scoped>
 h1, h2 {
-  font-size: 72px;
+  font-size: 60px;
   width: 100%;
   font-weight: normal;
 }
@@ -77,11 +84,31 @@ a {
   color: #42b983;
 }
 input {
-  width: 300px;
-  height: 100px;
   font-size: 26px;
 }
-div .studentNUM {
+.checkbox input {
+  width: 50px;
+  height:50px;
+}
+.checkbox {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+}
+.checkboxes {
+  width: 500px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+div .question textarea{
+  resize: none;
+  width:500px;
+  height:200px;
+}
+div .question {
   width: 100%;
   display: flex;
   flex-flow: column nowrap;
