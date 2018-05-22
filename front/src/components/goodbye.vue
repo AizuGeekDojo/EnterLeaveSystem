74671b61eb94fec4c600cdb9ff8ccbcf27d988a4
nozomi0966 <nozomotoitech@gmail.com>
2018-05-22 13:12:25 +0900

    add goodbyeだけど動かない　

---
+++ front/src/components/goodbye.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id='goodbye' class="container align-middle">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+import router from '../router'
+export default {
+  name: 'goodbye',
+  data () {
+    return {
+      message: 'Good bye'
+    }
+  },
+  mounted: function () {
+    setTimeout(function () {
+      router.push({name: 'top'})
+    }, 3000)
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  display: table-cell;
+  height: 100%;
+  width: 100%;
+  font-weight: normal;
+  text-align: center;
+  vertical-align: middle;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+div #welcome{
+  display: table;
+  text-align: center;
+  vertical-align: middle;
+}
+</style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import top from '@/components/top'
 import regist from '@/components/register'
 import welcome from '@/components/welcome'
+import goodbye from '@/components/goodbye'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/welcome',
       name: 'welcome',
       component: welcome
+    },
+    {
+      path: '/goodbye',
+      name: 'goodbye',
+      comment: goodbye
     }
   ]
 })
