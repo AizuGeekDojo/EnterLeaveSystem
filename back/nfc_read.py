d6c61cf916b6a1d6ff4b4f03dd011b6c83146c36
Y.N <s1240095@gmail.com>
2018-06-12 08:48:53 +0900

    Remove timeout.

--- back/nfc_read.py
+++ back/nfc_read.py
@@ -1,5 +1,5 @@
 import subprocess
 def nfc_read():
     p = subprocess.Popen("python2 nfc_reader.py", shell=True,stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout_data, stderr_data = p.communicate("".encode("utf-8"),timeout=1000)
+    stdout_data, stderr_data = p.communicate("".encode("utf-8"))
     return stdout_data.decode("utf-8").replace("\n", "")
