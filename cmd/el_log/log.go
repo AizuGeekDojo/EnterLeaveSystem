2f8e9be26141c7a50baa2b7bfd211316e3d4f156
wancom <softpiware@gmail.com>
2019-02-24 12:26:29 +0900

    Add utils

---
+++ cmd/el_log/log.go
@@ -0,0 +1,9 @@
+package el_log
+
+import "time"
+
+// AddLog adds enter/leave log
+// and change isenter status
+func AddLog(UID string, isInside bool, Timestamp time.Time, Ext string) {
+
+}
---
+++ cmd/el_slack/slack.go
@@ -0,0 +1,8 @@
+package el_slack
+
+import "time"
+
+// SlackNotify sends slack notification.
+func SlackNotify(Name string, UID string, isInside string, Timestamp time.Time, Ext string) {
+
+}
