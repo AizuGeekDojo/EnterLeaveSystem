d050a8a2d1b466251226f92b9ae3c9d777801132
wancom <softpiware@gmail.com>
2019-02-24 16:43:17 +0900

    Add slack

--- cmd/el_slack/slack.go
+++
@@ -1,8 +0,0 @@
-package el_slack
-
-import "time"
-
-// SlackNotify sends slack notification.
-func SlackNotify(Name string, UID string, isInside string, Timestamp time.Time, Ext string) {
-
-}
---
+++ cmd/slack/slack.go
@@ -0,0 +1,8 @@
+package slack
+
+import "time"
+
+// SlackNotify sends slack notification.
+func SlackNotify(Name string, UID string, isEnter bool, Timestamp time.Time, Ext string) {
+
+}
