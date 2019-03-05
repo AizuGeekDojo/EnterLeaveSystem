9cc67fa059ee03dca1fb3df68a0a0df13ca13edf
wancom <softpiware@gmail.com>
2019-03-05 09:52:39 +0900

    Remove http status code hardcode

--- cmd/handler/log.go
+++ cmd/handler/log.go
@@ -28,14 +28,13 @@ func LogAPIHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "OPTIONS" {
 		w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
-		w.WriteHeader(200)
 		return
 	}
 
 	if r.Method == "POST" {
 		addLogHandler(w, r)
 	} else {
-		w.WriteHeader(405)
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprintf(w, "Unexpected method")
 		log.Printf("%v %v: Unexpected method", r.Method, r.URL.Path)
 	}
@@ -45,7 +44,7 @@ func addLogHandler(w http.ResponseWriter, r *http.Request) {
 	var logdat LogInfo
 	reqlen, err := strconv.Atoi(r.Header.Get("Content-Length"))
 	if err != nil {
-		w.WriteHeader(400)
+		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "Cannot get Content-Length: %v", err)
 		log.Printf("%v %v: Bad request: %v", r.Method, r.URL.Path, err)
 		return
@@ -53,14 +52,14 @@ func addLogHandler(w http.ResponseWriter, r *http.Request) {
 	body := make([]byte, reqlen)
 	_, err = r.Body.Read(body)
 	if err != nil {
-		w.WriteHeader(400)
+		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "Failed to read: %v", err)
 		log.Printf("%v %v: Bad request: %v", r.Method, r.URL.Path, err)
 		return
 	}
 	err = json.Unmarshal(body, &logdat)
 	if err != nil {
-		w.WriteHeader(400)
+		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "Failed to parse JSON: %v", err)
 		log.Printf("%v %v: Bad request: %v", r.Method, r.URL.Path, err)
 		return
@@ -69,20 +68,20 @@ func addLogHandler(w http.ResponseWriter, r *http.Request) {
 	ts := time.Now()
 	name, _, err := db.GetUserInfo(logdat.UID)
 	if err != nil {
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "Internal server error: %v", err)
 		log.Printf("%v %v: db.GetUserInfo error: %v", r.Method, r.URL.Path, err)
 		return
 	}
 	if name == "" {
-		w.WriteHeader(404)
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "The ID is not found.")
 		return
 	}
 
 	err = db.AddLog(logdat.UID, (logdat.IsEnter == 1), ts, logdat.Ext)
 	if err != nil {
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "Internal server error: %v", err)
 		log.Printf("%v %v: db.AddLog error: %v", r.Method, r.URL.Path, err)
 		return
@@ -90,7 +89,7 @@ func addLogHandler(w http.ResponseWriter, r *http.Request) {
 
 	err = slack.Notify(name, logdat.UID, (logdat.IsEnter == 1), ts, logdat.Ext)
 	if err != nil {
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "Internal server error: %v", err)
 		log.Printf("%v %v: slack.Notify error: %v", r.Method, r.URL.Path, err)
 		return
--- cmd/handler/user.go
+++ cmd/handler/user.go
@@ -43,7 +43,6 @@ func UserAPIHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "OPTIONS" {
 		w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
-		w.WriteHeader(200)
 		return
 	}
 
@@ -53,7 +52,7 @@ func UserAPIHandler(w http.ResponseWriter, r *http.Request) {
 	case "POST":
 		createUserHandler(w, r)
 	default:
-		w.WriteHeader(405)
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		fmt.Fprintf(w, "Unexpected method")
 		log.Printf("%v %v: Unexpected method", r.Method, r.URL.Path)
 	}
@@ -68,17 +67,17 @@ func getUserHandler(w http.ResponseWriter, r *http.Request) {
 
 	username, isenter, err := db.GetUserInfo(uid)
 	if err != nil {
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
 		log.Printf("%v %v: db.GetUserInfo error: %v", r.Method, r.URL.Path, err)
 	} else if username == "" {
-		w.WriteHeader(404)
+		w.WriteHeader(http.StatusNotFound)
 	}
 	userresdat.UserName = username
 	userresdat.IsEnter = isenter
 
 	retbyte, err := json.Marshal(userresdat)
 	if err != nil {
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
 		log.Printf("%v %v: json.Marshal error: %v", r.Method, r.URL.Path, err)
 		fmt.Fprintf(w, "{}", err)
 	}
@@ -91,7 +90,7 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 
 	reqlen, err := strconv.Atoi(r.Header.Get("Content-Length"))
 	if err != nil {
-		w.WriteHeader(400)
+		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "Cannot get Content-Length: %v", err)
 		log.Printf("%v %v: Bad request: %v", r.Method, r.URL.Path, err)
 		return
@@ -99,14 +98,14 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 	body := make([]byte, reqlen)
 	_, err = r.Body.Read(body)
 	if err != nil {
-		w.WriteHeader(400)
+		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "Failed to read: %v", err)
 		log.Printf("%v %v: Bad request: %v", r.Method, r.URL.Path, err)
 		return
 	}
 	err = json.Unmarshal(body, &userdat)
 	if err != nil {
-		w.WriteHeader(400)
+		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "Failed to parse JSON: %v", err)
 		log.Printf("%v %v: Bad request: %v", r.Method, r.URL.Path, err)
 		return
@@ -120,12 +119,12 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 		userresdat.Success = false
 		userresdat.Reason = fmt.Sprintf("%v\n", err)
 		log.Printf("%v %v: Bad request: %v", r.Method, r.URL.Path, err)
-		w.WriteHeader(400)
+		w.WriteHeader(http.StatusBadRequest)
 	}
 
 	retbyte, err := json.Marshal(userresdat)
 	if err != nil {
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError)
 		log.Printf("%v %v: json.Marshal error: %v", r.Method, r.URL.Path, err)
 		fmt.Fprintf(w, "{}", err)
 	}
