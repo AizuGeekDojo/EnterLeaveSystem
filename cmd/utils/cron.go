d0e026d93ffc8018ecef63530b4eae4a04315ecf
wancom <softpiware@gmail.com>
2019-06-27 09:38:56 +0900

    Refacter

--- cmd/utils/cron.go
+++ cmd/utils/cron.go
@@ -13,26 +13,34 @@ func CronInit(d *sql.DB) error {
 	c := cron.New()
 	// sec min hour date month week
 	// 0 0 * * * * -> 0:0:* */* (*) Every month, day, hour and 0min, 0sec == Every hour
-	_, err := c.AddFunc("0 0 0 * * *", func() {
+	_, err := c.AddFunc("0 0 0 * * *", cronForceLeave(d))
+	if err != nil {
+		return err
+	}
+	_, err = c.AddFunc("0 0 0 1 * *", cronSendMonthlyLog(d))
+	if err != nil {
+		return err
+	}
+	c.Start()
+	return nil
+}
+
+func cronForceLeave(d *sql.DB) func() {
+	return func() {
 		err := db.ForceLeave(d)
 		if err != nil {
 			log.Printf("Cron: db.ForceLeave error: %v\n", err)
 		}
-	})
-	if err != nil {
-		return err
 	}
-	_, err = c.AddFunc("0 0 0 1 * *", func() {
+}
+
+func cronSendMonthlyLog(d *sql.DB) func() {
+	return func() {
 		err := SendMonthlyLog(d)
 		if err != nil {
 			log.Printf("Cron: db.ForceLeave error: %v\n", err)
 		} else {
 			log.Printf("Monthly log exported successfully!\n")
 		}
-	})
-	if err != nil {
-		return err
 	}
-	c.Start()
-	return nil
 }
