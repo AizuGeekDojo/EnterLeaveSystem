b65593e83c89353903cb8d2f2a348568d6092054
wancom <softpiware@gmail.com>
2019-07-12 11:36:47 +0900

    Modify install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -13,35 +13,15 @@ echo "Installing python and environment..."
 
 apt install -y python2.7 python-pip
 
-sudo apt install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-
-wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tar.xz
-tar xf Python-3.6.0.tar.xz
-cd Python-3.6.0
-./configure
-make
-sudo make altinstall
-cd ../
-
 python2 -m pip install --upgrade pip
 python2 -m pip install nfcpy
-python3 -m pip install --upgrade pip
-python3 -m pip install -r back/requirements.txt
 
 echo "Installing service..."
 echo "Copying files..."
-# cp enterleavesystemd.service /etc/systemd/system/
-# cp enterleavesystemhttpd.service /etc/systemd/system/
 cp elsystemd.service /etc/systemd/system/
 
 echo "Enabling Services..."
 systemctl daemon-reload
-# systemctl enable enterleavesystemd.service
-# systemctl start enterleavesystemd.service
-
-# systemctl enable enterleavesystemhttpd.service
-# systemctl start enterleavesystemhttpd.service
-
 systemctl enable elsystemd.service
 systemctl start elsystemd.service
 
