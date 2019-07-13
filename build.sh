535b3799d8668b218488c9ea5f278a504fb83d9f
wancom <softpiware@gmail.com>
2019-07-13 22:46:47 +0900

    Fix go env

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ else
   ELS_VER=`git describe --tags  --abbrev=0`
   if [[ ${ELS_VER} == *-* ]]; then
     git tag -d ${ELS_VER}
+    git push origin　${ELS_VER}
   fi
   ELS_VER=`git describe --tags  --abbrev=0`
   ELS_NVER="${ELS_VER%.*}.`expr ${ELS_VER##*.} + 1`-${CIRCLE_BRANCH}"
@@ -15,4 +16,7 @@ fi
 echo "${ELS_VER} -> ${ELS_NVER}"
 git tag ${ELS_NVER}
 git push --tags
-GOARCH="amd64" GOOS="linux" go run release.go ${CIRCLE_BRANCH} ${ELS_NVER}
+export GOARCH="amd64"
+export GOOS="linux"
+go env
+go run release.go ${CIRCLE_BRANCH} ${ELS_NVER}
