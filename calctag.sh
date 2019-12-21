64797be79d1edabafe1b8a4b5b09e4fa189d0dea
wancom <softpiware@gmail.com>
2019-12-21 20:41:17 +0900

    Debug

--- calctag.sh
+++ calctag.sh
@@ -19,28 +19,19 @@ else
 # If branch is not master(pull req), use same version
   ELS_VER=`git describe --tags  --abbrev=0`
   if [[ ${ELS_VER} == *-* ]]; then
-echo 10
   # If the version is already used, delete that.
     git tag -d ${ELS_VER}
-echo 11
     git push origin :${ELS_VER}
   fi
-echo 12
   # Set version
   ELS_VER=`git describe --tags  --abbrev=0`
-echo 13
   ELS_NVER="${ELS_VER%.*}.`expr ${ELS_VER##*.} + 1`-${BRANCHNAME}"
 fi
-echo 14
 echo "${ELS_VER} -> ${ELS_NVER}"
-echo 15
 printf ${ELS_NVER} > tagver
 # Set version tag
-echo 16
 git tag ${ELS_NVER}
-echo 17
 git push --tags
-echo 18
 # Release binary
 export GOARCH="amd64"
 export GOOS="linux"
