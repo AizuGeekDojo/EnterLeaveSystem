6b34dc1ccee660849133e570fbce4c689b073e1c
wancom <softpiware@gmail.com>
2020-01-12 13:08:10 +0900

    Modify path

--- server/handler/socket.go
+++ server/handler/socket.go
@@ -29,7 +29,7 @@ var clients = []*websocket.Conn{}
 func ReadCard(d *sql.DB) {
 	for {
 		var resdat IDCardInfo
-		dat, err := exec.Command("python2.7", "nfc_reader.py").Output()
+		dat, err := exec.Command("nfc_reader").Output()
 		if err != nil {
 			log.Printf("socket: nfc reader error : %v\n", err)
 
