8201604b3ac58290884c03fd6eefe85cc667a15d
Kai Yamada <gpioblink@aol.com>
2020-11-12 16:11:55 +0900

    update sql for testing

--- server/test/init.sql
+++ server/test/init.sql
@@ -3,6 +3,9 @@ BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "users" (sid TEXT,name TEXT,isenter INTEGER);
 INSERT INTO users VALUES('s1240095','Yusuke Namiki',0);
 CREATE TABLE IF NOT EXISTS "idcard" (idm TEXT,sid TEXT);
+CREATE TABLE IF NOT EXISTS "products" (id TEXT,name TEXT,barcode TEXT,borrowersid TEXT);
+INSERT INTO products VALUES('1','テスト商品A','','s1240095');
+INSERT INTO products VALUES('2','テスト商品B','12345678','s1240095');
 CREATE TABLE IF NOT EXISTS "log" (sid TEXT,isenter INTEGER,time INTEGER,ext TEXT);
 COMMIT;
 
