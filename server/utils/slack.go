fab3e3f36a0d40a890f119f418981f9a3a5c7ebd
kashu <64694079+kashu-02@users.noreply.github.com>
2025-12-16 01:49:04 +0900

    refactor: SlackNotify関数の簡素化と不要な構造体の削除
    - WebHookおよびAttachment構造体を削除し、slack-goライブラリのWebhookMessageを直接使用するように変更
    - SlackNotify関数内の変数名を小文字に統一し、可読性を向上
    - エラーハンドリングのメッセージを改善

--- server/utils/slack.go
+++ server/utils/slack.go
@@ -4,110 +4,52 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
 	"time"
+
+	"github.com/slack-go/slack"
 )
 
-// WebHook is structure for slack notify
-type WebHook struct {
-	Channel     string       `json:"channel"`
-	Username    string       `json:"username"`
-	Text        string       `json:"text"`
-	IconEmoji   string       `json:"icon_emoji"`
-	Attachments []Attachment `json:"attachments"`
-}
-
-// Attachment is structure for slack notify data
-type Attachment struct {
-	Title   string `json:"title"`
-	Pretext string `json:"pretext"`
-	Text    string `json:"text"`
-}
-
-// WebHookInit returns new WebHook data
-func WebHookInit(cfg *SlackInfo) *WebHook {
-	return &WebHook{
-		Username:  cfg.UserName,
-		IconEmoji: cfg.IconEmoji,
-		Channel:   cfg.Channel,
-	}
-}
-
-// SlackNotify sends slack notification.
-func SlackNotify(Name string, UID string, isEnter bool, Timestamp time.Time, Ext string) error {
-
+// SlackNotify sends Slack notification using slack-go webhook client.
+func SlackNotify(name string, uid string, isEnter bool, timestamp time.Time, ext string) error {
 	cfg := GetSlackInfo()
-	HookJSON := WebHookInit(&cfg)
-
-	var io string
-	if isEnter {
-		io = "入室"
-	} else {
-		io = "退室"
-	}
-
-	HookJSON.Text = fmt.Sprintf("%v : %v さんが %v に%vしました。", UID, Name, Timestamp.Format("2006-01-02 15:04:05"), io)
-
-	if Ext != "" {
-		var RawJSON = []byte(Ext)
-		var ExtList = make(map[string]interface{})
-
-		err := json.Unmarshal(RawJSON, &ExtList)
-		if err != nil {
-			log.Println(err)
-			log.Println("Error has occured in SlackNotify. User Ext is : ", Ext)
-			return err
-		}
-
-		useage := ExtList["Use"].([]interface{})
-		mess := ExtList["message"].(string)
-
-		HookJSON.Attachments = append(HookJSON.Attachments, Attachment{
-			Title: "アンケート結果",
-			Text:  fmt.Sprintf("目的 : %v \n 感想 : %v", useage, mess),
-		})
-	}
-
-	err := postEnterLeaveLog(HookJSON)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-	return nil
-}
-
-func postEnterLeaveLog(ellog *WebHook) error {
-	IncomingURL := GetSlackInfo().GetWebHookURL()
-	if IncomingURL == "" {
+	webhookURL := cfg.GetWebHookURL()
+	if webhookURL == "" {
 		return errors.New("Slack URL is not defined")
 	}
 
-	params, err := json.Marshal(ellog)
-	if err != nil {
+	status := "退室"
+	if isEnter {
+		status = "入室"
+	}
+
+	message := &slack.WebhookMessage{
+		Channel:   cfg.Channel,
+		Username:  cfg.UserName,
+		IconEmoji: cfg.IconEmoji,
+		Text:      fmt.Sprintf("%v : %v さんが %v に%vしました。", uid, name, timestamp.Format("2006-01-02 15:04:05"), status),
+	}
+
+	if ext != "" {
+		var extMap map[string]interface{}
+		if err := json.Unmarshal([]byte(ext), &extMap); err != nil {
+			log.Println(err)
+			log.Println("Error has occured in SlackNotify. User Ext is : ", ext)
+			return err
+		}
+
+		usage := extMap["Use"]
+		mess := extMap["message"]
+		message.Attachments = append(message.Attachments, slack.Attachment{
+			Title: "アンケート結果",
+			Text:  fmt.Sprintf("目的 : %v \n 感想 : %v", usage, mess),
+		})
+	}
+
+	if err := slack.PostWebhook(webhookURL, message); err != nil {
+		log.Println(err)
 		return err
 	}
 
-	client := http.Client{
-		Timeout: time.Duration(5 * time.Second),
-	}
-
-	resp, err := client.PostForm(
-		IncomingURL,
-		url.Values{"payload": {string(params)}},
-	)
-	if err != nil {
-		return err
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	println(string(body))
 	return nil
 }
