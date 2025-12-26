4a7657899bc28eb7129324be94f1c1060f3f5492
kashu <64694079+kashu-02@users.noreply.github.com>
2025-12-26 19:35:56 +0900

    chore: move update_user.sh from bin/ to /

--- update_user.sh
+++ update_user.sh
@@ -7,7 +7,7 @@ read USERID
 
 ssh ${USERID}@${SERVER} getent passwd |awk -F: '{print $1 "|" $5 "|0"}' > users_sqlite
 
-sqlite3 ../database.db << EOF
+sqlite3 ./database.db << EOF
 delete from users;
 .import users_sqlite users
 EOF
